538a000627d332bfabb69e567766461774eee2ee
Adrian Yang <69127002+adrianyang2012@users.noreply.github.com>
2023-12-16 20:05:25 -0800

    Update minecraft_game.py

--- minecraft_game.py
+++ minecraft_game.py
@@ -3,6 +3,9 @@ import math
 import pygame
 # initialising pygame
 pygame.init()
+pygame.font.init()
+my_font = pygame.font.SysFont('Comic Sans MS', 30)
+
 import random
 battle = 0
 
@@ -30,8 +33,8 @@ enemy_battle = pygame.image.load('Minecraft_game\Minecraft_game\Pig_battling.web
 enemy_battle = pygame.transform.scale(enemy_battle, (100,100))
 big_player = pygame.transform.scale(image, (100,100))
 # creating a running loop
+battle = 0
 while running:
-       
     # creating a loop to check events that 
     # are occurring
 
@@ -41,28 +44,37 @@ while running:
             if event.type == pygame.QUIT:
                 # change the value to False, to exit the main loop
                 running = False
-            if keys[pygame.K_w] or keys[pygame.K_UP]:
-              py -=3
-            if keys[pygame.K_s] or keys[pygame.K_DOWN]:
-              py+=3
-            if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-              px -=3
-            if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-              px+=3
-    p_attacking = 0    
+            if not battle:
+              if keys[pygame.K_w] or keys[pygame.K_UP]:
+                py -=3
+              if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+                py+=3
+              if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+                px -=3
+              if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+                px+=3
     if battle:
-      en_health = random.randint(12,15)
+
+
       if attacker == 'e':
-        en_x-=0.5
+        if en_attacking:
+          en_x-=0.5
+          if en_x < 100:
+            health -= 1
+            attacker = 'p'
+            p_attacking = 0
 
-          
-        if en_x < 100:
-          attacker = 'p'
-
-        if en_x < 150:
-          p_x-=0.5  
+          if en_x < 150:
+            p_x-=0.5
+        else:
+          if p_x > 100:
+            p_x -= 0.4
+          else:
+            en_attacking = 1
+          if p_x > 350:
+            en_x -= 0.4  
       else:
-        p_attacking = 0
+        
         if en_x < 400:
         
           en_x+=0.4
@@ -74,30 +86,41 @@ while running:
           p_x+=0.5
           if p_x > 400:
             p_attacking = 0
+            en_attacking = 0
             attacker = 'e'
+            en_health -=1
           
 
 
           if p_x > 350:
-            en_x+=0.5  
-
+            en_x+=0.5
+        if en_health < 1 or health < 1:
+          battle = 0
+      
       screen.fill((0,0,255))
+      text_surface = my_font.render(str(health), False, (255-health*11, health*11, 0))
+      screen.blit(text_surface, (p_x+20,450))
+      text_surface2 = my_font.render(str(en_health), False, (255-health*11, health*11, 0))
+      screen.blit(text_surface2, (en_x+20,450))
       screen.blit(enemy_battle,(en_x,520))
       screen.blit(big_player,(p_x,500))
 
     else:        
       screen.fill((0,255,0))
-      battle = 0
+      
       screen.blit(image, (px,py))
       for enemy in enemys:
         screen.blit(enemy_image,(enemy[0],enemy[1]))
         if math.sqrt((enemy[0]-px)**2 + (enemy[1]-py)**2) < 30:
           en_x = 400
+          enemys.remove(enemy)
+          en_health = random.randint(10,12)
           p_x = 100
+          p_attacking = 0
+          en_attacking = 1
           battle = 1
           attacker = 'e'
 
           
-
     pygame.display.flip()
     
